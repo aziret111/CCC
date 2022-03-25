f56599e387d209c30d5b99663ed4f87c1e0e92d9
ashymov <sashymov@gmail.com>
2022-03-25 15:59:59 +0600

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace CCC { 
+    class Program {
+        static void Main() {
+            // byte number = 10;
+            // short number = 32001;
+            // int number = 33000;
+            long number1 = 100000;
+            
+            // float num = 4.555f;
+            //double num_2 = 5.665d;
+
+            string word = "Peremennaya: ";
+
+            // char symbol = '8';
+
+            bool isHappy = false;
+
+                
+            Console.WriteLine(word  + number1 + ".");
+
+
+            // gfigusdbiu
+            // Console.ReadKey();
+
+        }
+    } 
+
+}
